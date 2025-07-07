25618c42d60eb74371dfef4a77f977fc02dde1c9
gustavomartinezx <gustavopalmr@gmail.com>
2025-07-06 21:14:33 -0300

    ♻️ chore: configurações para deploy mudando as settings para requisições do front

--- back/src/main/java/br/com/printers/back/config/CorsConfig.java
+++ back/src/main/java/br/com/printers/back/config/CorsConfig.java
@@ -2,21 +2,42 @@ package br.com.printers.back.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins("http://localhost:3000", "https://desafio-tecnico-m4all-1.onrender.com") 
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
+            }
+        };
+    }
+
+    @Bean
+    public OncePerRequestFilter privateNetworkAccessFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
+                    throws ServletException, IOException {
+                
+                response.setHeader("Access-Control-Allow-Private-Network", "true");
+                filterChain.doFilter(request, response);
             }
         };
     }
